f1b6169a166c579d86c46647cf64b99740e1ca63
ceko1 <cccceeekko@yahoo.com>
2016-08-29 18:07:54 -0500

    working adb command adb devices, I had to comment out the scrollTo methods

--- src/main/java/core/ADB.java
+++ src/main/java/core/ADB.java
@@ -1,14 +1,25 @@
 package core;
 
+import core.managers.ServerManager;
+import org.junit.Test;
+
 /**
  * Created by ceko on 08/28/2016.
  */
 public class ADB {
 
+    public static String command(String command){
+        if(command.startsWith("adb")) command = command.replace("adb ", ServerManager.getAndroidHome()+"/platform-tools/adb ");
+        else throw new RuntimeException("This method is designed to run adb commands only");
+        String output = ServerManager.runCommand(command);
+        if(output == null) return "";
+        else return output;
+    }
 
-
-
-
+    @Test
+    public void test(){
+        System.out.println(command("adb devices"));
+    }
 
 
 }
--- src/main/java/core/UiObject.java
+++ src/main/java/core/UiObject.java
@@ -1,6 +1,10 @@
 package core;
 
+//he has api.android.Android and a method scrollTo() but this library does not have it I have to implement it there are some examples online
+//scrollTo() is removed
 import api.android.some.Android;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebElement;
@@ -17,6 +21,13 @@ public class UiObject {
         System.out.println(this.locator);
 
     }
+
+    //missing scrollTo method my implementation
+    //public MobileElement scrollTo(String text) {
+    //    String uiScrollables = AndroidDriver.UiScrollable("new UiSelector().descriptionContains(\"" + text + "\")") +
+    //            AndroidDriver.UiScrollable("new UiSelector().textContains(\"" + text + "\")");
+    //    return (MobileElement) findElementByAndroidUIAutomator(uiScrollables);
+    //}
 
     //distinguish between xpath and uiselector from UiSelector class objects
     private boolean isXpath(){
@@ -199,7 +210,56 @@ public class UiObject {
             if(isXpath()) element = Android.driver.findElementByXPath(locator);
             else element = Android.driver.findElementByAndroidUIAutomator(locator);
             return element.getAttribute("contentDesc");
-        
+
+    }
+
+    public UiObject clearText(){
+
+        if(isXpath()) Android.driver.findElementByXPath(locator).clear();
+        else  Android.driver.findElementByAndroidUIAutomator(locator).clear();
+        return this;
+    }
+
+    public UiObject typeText(String value){
+
+        if(isXpath()) Android.driver.findElementByXPath(locator).sendKeys(value);
+        else  Android.driver.findElementByAndroidUIAutomator(locator).sendKeys(value);
+        return this;
+    }
+
+    public UiObject tap(){
+
+        if(isXpath()) Android.driver.findElementByXPath(locator).click();
+        else  Android.driver.findElementByAndroidUIAutomator(locator).click();
+        return this;
+    }
+
+    //public UiObject scrollTo(){
+    //    if(locator.contains("text")) throw new RuntimeException("Scroll to method can only be used with text attributes and current locator: "+locator+" does not contain any text attributes!");
+    //    if(isXpath()) Android.driver.scrollTo(locator.substring(locator.indexOf("@text=\""), locator.indexOf("\"]")).replace("@text=\"", ""));
+    //    else{
+    //        String text;
+    //        if(locator.contains("textContains")) text = locator.substring(locator.indexOf(".textContains(\""), locator.indexOf("\")")).replace(".textContains(\"", "");
+    //        else text = locator.substring(locator.indexOf(".textContains(\""), locator.indexOf("\")")).replace(".textContains(\"", "");
+    //        Android.driver.scrollTo(text);
+    //    }
+    //    return this;
+    //}
+
+    public UiObject waitToAppear(int seconds){
+        Timer timer = new Timer();
+        timer.start();
+        while(!timer.expired(seconds)) if(exists()) break;
+        if(timer.expired(seconds) && !exists()) throw new AssertionError("Element "+locator+" failed to appear within "+seconds+" seconds");
+        return this;
+    }
+
+    public UiObject waitToDisappear(int seconds){
+        Timer timer = new Timer();
+        timer.start();
+        while(!timer.expired(seconds)) if(!exists()) break;
+        if(timer.expired(seconds) && exists()) throw new AssertionError("Element "+locator+" failed to disappear within "+seconds+" seconds");
+        return this;
     }
 
 

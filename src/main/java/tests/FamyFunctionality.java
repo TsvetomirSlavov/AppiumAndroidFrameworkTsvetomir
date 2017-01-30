84d523da25a6079667490922068a453a10327878
ceko1 <cccceeekko@yahoo.com>
2017-01-30 02:40:28 -0600

    1 30 2017

--- src/main/java/tests/FamyFunctionality.java
+++ src/main/java/tests/FamyFunctionality.java
@@ -10,6 +10,11 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static api.android.Android.driver;
 
 /**
  * Test Suite To Test FamyFunctionality of Famy app
@@ -31,11 +36,16 @@ public class FamyFunctionality extends TestManager{
     }
 
     @Test
-    public void swipeThroughGroups(){
+    public void swipeThroughGroups() throws InterruptedException {
+        //WebDriverWait wait = new WebDriverWait(driver, 30);
+        //wait.until(ExpectedConditions.elementToBeClickable(By
+        //        .xpath("//android.widget.Button[contains(@text, 'Log In')]")));
+        //Android.driver.wait(6);
+        //Thread.sleep(15000);
         testInfo.id("swipe through groups").suite("FamyFunctionality").name("Verify that you can swipe through groups");
-        System.out.println(Android.driver.findElementByName("ceko").getText());
-        Assert.assertTrue(Android.driver.findElementByName("ceko").isDisplayed());
-        Android.driver.swipe(800,1500,900,600,1);
-        Assert.assertFalse(Android.driver.findElementByName("ceko").isDisplayed());
+        System.out.println(driver.findElementByName("ceko").getText());
+        Assert.assertTrue(driver.findElementByName("ceko").isDisplayed());
+        driver.swipe(800,1500,900,600,1);
+        Assert.assertFalse(driver.findElementByName("ceko").isDisplayed());
     }
 }
